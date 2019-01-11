42b0422aef411b2aab11832c7bcbd3c0720053a2
rajatrao777 <rajatrao777@gmail.com>
2019-01-11 17:18:27 +0530

    background v1.0

--- src/Background.js
+++ src/Background.js
@@ -5,11 +5,10 @@ import './style.css';
 const Background = () => {
     return (
         <div className = "container">
-            <div className = "container__layout">R</div>
+            <div className = "container__layout"></div>
         </div>
     )
 
 }
-
 
 export default Background;
