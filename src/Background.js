fb5722878c37126cdada2adf0ffee86f15624aa7
rajatrao777 <rajatrao777@gmail.com>
2019-01-11 23:05:08 +0530

    translateX code

--- src/Background.js
+++ src/Background.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import './style.css';
 
+//  const Styling = ()=>{
+//      return (<div className = "slider"></div>)
+//  }
 
-const Background = () => {
-    return (
-        <div className = "container">
-            <div className = "container__layout">
-                <div className = "container__bar"></div>
+class Background extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { color: '', translate: '' }
+        this.slide = this.slide.bind(this);
+
+    }
+
+    slide() {
+        this.setState({ color: "blue", translate: "translateX(50px)" })
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="container__layout">
+                    <div className="container__bar"></div>
+                    <div className="container__emoji">
+                        <div className="container__emoji-slider" onClick={this.slide} style={{ background: this.state.color, transform: this.state.translate }}></div>
+                    </div>
+                </div>
             </div>
-        </div>
-    )
-
+        )
+    }
 }
 
 export default Background;
