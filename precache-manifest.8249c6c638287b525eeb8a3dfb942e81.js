f10f1de055f2ce8e8f9ecf5967108dcc8a592a79
rajatrao777 <rajatrao777@gmail.com>
2019-01-11 11:33:28 +0530

    Updates

---
+++ precache-manifest.8249c6c638287b525eeb8a3dfb942e81.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "6708eb8ade55a32c6cdc",
+    "url": "/Emojimeter/static/js/runtime~main.6708eb8a.js"
+  },
+  {
+    "revision": "ce5092eed78fb32c7a08",
+    "url": "/Emojimeter/static/js/main.ce5092ee.chunk.js"
+  },
+  {
+    "revision": "a44f0c0078e70b51841d",
+    "url": "/Emojimeter/static/js/1.a44f0c00.chunk.js"
+  },
+  {
+    "revision": "9731cefec2e07dee2b33b6a9fd9c2bbc",
+    "url": "/Emojimeter/index.html"
+  }
+];
