98e502c971bb177745cbe0bf4fc841c1a4e62f95
rajatrao777 <rajatrao777@gmail.com>
2019-01-11 11:05:23 +0530

    added base code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+class App extends React.Component {
+    render() {
+        return (
+            <h1>{this.props.name}</h1>
+        )
+    }
+}
+
+
+//Always write ReactDOM.render code at the last and as it might not find func initially and throw error 
+//Serial execution is performed 
+ReactDOM.render(
+    <App name="Hey" />,
+    document.getElementById('root')
+)
