32c4383d15dd3621194153677c3e607b0431ddb1
rajatrao777 <rajatrao777@gmail.com>
2019-01-12 19:04:20 +0530

    added code need changes

--- src/Background.js
+++ src/Background.js
@@ -6,13 +6,16 @@ import './style.css';
 //  }
 
 class Background extends React.Component {
-    
+
     constructor(props) {
         super(props);
-        this.state = { color: '#facd49' }
-        console.log(this.state.color+"start");
+        this.state = { color: '#facd49', mousedown: false,start:'',translateX:''}
+        console.log(this.state.start + "start");
         // this.state = { x: '', y: '' };
-        this.slide = this.slide.bind(this);
+        this.down = this.down.bind(this);
+        this.leave = this.leave.bind(this);
+        this.up = this.up.bind(this);
+        this.move = this.move.bind(this);
         
     }
     // componentDidMount(){
@@ -25,25 +28,65 @@ class Background extends React.Component {
     //     // this.setState({ color: "blue", translate: "translateX(50px)" });
     //     alert("Clicked");
     // }
-    slide() {
-        
-        var c;
-        c =  this.state.color==='#facd49'?'blue':'#facd49';
+    // change(e) {
 
-        console.log(this.state.color+"click");
-        this.setState({color:c})
-        console.log(this.state.color+"clickstateafter");
+    //     // var c;
+    //     // c =  this.state.color==='#facd49'?'blue':'#facd49';
+    //     //can be used directly color:this.state.color==='#facd49'?'blue':'#facd49';
+    //     console.log(this.state.color + "click");
+    //     // console.log(e.ClientX,e.ClientY);
+    //     this.setState(state => ({ color: state.color === '#facd49' ? 'blue' : '#facd49' }))
+    //     console.log(this.state.color + "clickstateafter");
+
+    // }
+
+    // click(e){
+
+    //    this.setState({ x: e.screenX, y: e.screenY });
+    //     console.log(this.state.y);
+    // }
+    down(event){
+        
+        this.setState({ mousedown: true ,start :event.pageX});
+        
+        console.log(this.state.start+"chalu");
+
     }
 
+    leave() {
+        this.setState({ mousedown: false })
+        
+    }
+    up(event){
+        this.setState({ mousedown: false ,start :event.pageX})
+        console.log(event.pageX+"leave");
+    }
+    move(event){
+        if(!this.state.mousedown) return;
+        event.preventDefault();
+        // console.log(event.pageX+" "+this.state.start);
+        console.log("pagex"+event.pageX);
+        console.log("mstart"+this.state.start);
+        var mo = this.state.start;
+        var page = event.pageX;
+        var dist = page-mo;
+        this.setState({translateX:dist});
+        console.log(page-mo+"move");
+        
+        
+        // console.log(this.state.mousedown+"move");
+    }
     render() {
-        console.log(this.state.color+"render");
+        // const { x, y } = this.state;
+        // console.log(this.state.mousedown+"down");
+        // console.log(this.state.mousedown+"up");
         return (
             
             <div className="container">
                 <div className="container__layout">
                     <div className="container__bar"></div>
                     <div className="container__emoji">
-                        <div className="container__emoji-slider" onClick={this.slide} style={{background:this.state.color}} ></div>
+                        <div className="container__emoji-slider" onMouseDown={this.down} onMouseLeave={this.leave} onMouseUp={this.up} onMouseMove={(event)=>{this.move(event)}} style={{ transform: `translateX(${this.state.translateX}px)`}} ></div>
                         
                     </div>
                 </div>
