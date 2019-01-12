fee2e53423293e3dd705bd6e6a93ec629a286570
rajatrao777 <rajatrao777@gmail.com>
2019-01-12 11:23:20 +0530

    added oppo colors 2

--- src/Background.js
+++ src/Background.js
@@ -6,24 +6,45 @@ import './style.css';
 //  }
 
 class Background extends React.Component {
+    
     constructor(props) {
         super(props);
-        this.state = { color: '', translate: '' }
+        this.state = { color: '#facd49' }
+        console.log(this.state.color+"start");
+        // this.state = { x: '', y: '' };
         this.slide = this.slide.bind(this);
-
+        
     }
-
+    // componentDidMount(){
+    //     document.addEventListener('click',this.Drag);
+    // }
+    // componentWillUnmount(){
+    //     document.addEventListener('click',this.Drag);
+    // }
+    // Drag(){
+    //     // this.setState({ color: "blue", translate: "translateX(50px)" });
+    //     alert("Clicked");
+    // }
     slide() {
-        this.setState({ color: "blue", translate: "translateX(50px)" })
+        
+        var c;
+        c =  this.state.color==='#facd49'?'blue':'#facd49';
+
+        console.log(this.state.color+"click");
+        this.setState({color:c})
+        console.log(this.state.color+"clickstateafter");
     }
 
     render() {
+        console.log(this.state.color+"render");
         return (
+            
             <div className="container">
                 <div className="container__layout">
                     <div className="container__bar"></div>
                     <div className="container__emoji">
-                        <div className="container__emoji-slider" onClick={this.slide} style={{ background: this.state.color, transform: this.state.translate }}></div>
+                        <div className="container__emoji-slider" onClick={this.slide} style={{background:this.state.color}} ></div>
+                        
                     </div>
                 </div>
             </div>
