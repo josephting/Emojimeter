86a6841c34eb24fe874e2b1158a63618db9166fa
rajatrao777 <rajatrao777@gmail.com>
2019-01-12 21:52:05 +0530

    debugging.............

--- src/Background.js
+++ src/Background.js
@@ -9,14 +9,15 @@ class Background extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = { color: '#facd49', mousedown: false,start:'',translateX:''}
-        console.log(this.state.start + "start");
+        this.state = { color: '#facd49', mousedown: false, start: '', translateX: '' }
+        // console.log(this.state.start + "start");
+        //console.log(this.state.translateX+"X");
         // this.state = { x: '', y: '' };
         this.down = this.down.bind(this);
-        this.leave = this.leave.bind(this);
+        // this.leave = this.leave.bind(this);
         this.up = this.up.bind(this);
         this.move = this.move.bind(this);
-        
+
     }
     // componentDidMount(){
     //     document.addEventListener('click',this.Drag);
@@ -45,49 +46,53 @@ class Background extends React.Component {
     //    this.setState({ x: e.screenX, y: e.screenY });
     //     console.log(this.state.y);
     // }
-    down(event){
-        
-        this.setState({ mousedown: true ,start :event.pageX});
-        
-        console.log(this.state.start+"chalu");
+    down(a) {
+
+        this.setState({ mousedown: true, start: a.pageX });
+
+        console.log(a.pageX + "chalu");
 
     }
 
-    leave() {
-        this.setState({ mousedown: false })
-        
+    // leave() {
+    //     this.setState({ mousedown: false })
+
+    // }
+    up(b) {
+        this.setState({ mousedown: false})
+        console.log(b.pageX + " "+this.state.start+"leave");
     }
-    up(event){
-        this.setState({ mousedown: false ,start :event.pageX})
-        console.log(event.pageX+"leave");
-    }
-    move(event){
-        if(!this.state.mousedown) return;
-        event.preventDefault();
-        // console.log(event.pageX+" "+this.state.start);
-        console.log("pagex"+event.pageX);
-        console.log("mstart"+this.state.start);
-        var mo = this.state.start;
-        var page = event.pageX;
-        var dist = page-mo;
-        this.setState({translateX:dist});
-        console.log(page-mo+"move");
-        
-        
+    move(event) {
+        //console.log(this.state.mousedown+"boolean");
+        if (!this.state.mousedown) return;
+        else {
+            event.preventDefault();
+            // console.log(event.pageX+" "+this.state.start);
+            console.log("pagex" + event.pageX);
+            console.log("mstart" + this.state.start);
+            const mo = this.state.start;
+            const page = event.pageX;
+            const dist = page - mo;
+            console.log(dist);
+            this.setState({ translateX: dist });
+            // console.log(page - mo + "move");
+        }
+
         // console.log(this.state.mousedown+"move");
     }
     render() {
         // const { x, y } = this.state;
         // console.log(this.state.mousedown+"down");
-        // console.log(this.state.mousedown+"up");
+        //console.log(this.state.translateX+"X");
+        console.log(this.state.start+"render");
         return (
-            
+
             <div className="container">
                 <div className="container__layout">
                     <div className="container__bar"></div>
                     <div className="container__emoji">
-                        <div className="container__emoji-slider" onMouseDown={this.down} onMouseLeave={this.leave} onMouseUp={this.up} onMouseMove={(event)=>{this.move(event)}} style={{ transform: `translateX(${this.state.translateX}px)`}} ></div>
-                        
+                        <div className="container__emoji-slider" onMouseDown={this.down}  onMouseUp={this.up} onMouseMove={(event)=>{this.move(event)}} style={{ transform: `translateX(${this.state.translateX}px)` }} >{this.state.translateX}</div>
+                    
                     </div>
                 </div>
             </div>
