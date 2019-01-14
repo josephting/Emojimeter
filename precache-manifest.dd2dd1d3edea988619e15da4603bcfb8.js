4c7bf7ea952b6b42d329a9b8f8ecc2b573e25a82
rajatrao777 <rajatrao777@gmail.com>
2019-01-14 18:18:11 +0530

    Updates

--- precache-manifest.dd2dd1d3edea988619e15da4603bcfb8.js
+++ precache-manifest.dd2dd1d3edea988619e15da4603bcfb8.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/Emojimeter/static/js/runtime~main.6708eb8a.js"
   },
   {
-    "revision": "4f98b159574eb4eac59a",
-    "url": "/Emojimeter/static/js/main.4f98b159.chunk.js"
+    "revision": "44919c16d31d43fbe2cb",
+    "url": "/Emojimeter/static/js/main.44919c16.chunk.js"
   },
   {
     "revision": "fbfa9ac1c04d6f818c50",
     "url": "/Emojimeter/static/js/1.fbfa9ac1.chunk.js"
   },
   {
-    "revision": "4f98b159574eb4eac59a",
+    "revision": "44919c16d31d43fbe2cb",
     "url": "/Emojimeter/static/css/main.222f3a0e.chunk.css"
   },
   {
-    "revision": "1f125caa9dfa5ba663711c0bdd25b48a",
+    "revision": "cfe5a7bc193f36c3b73e9d7d29804665",
     "url": "/Emojimeter/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Emojimeter/precache-manifest.4a711587b5df6db3ae3b5381f1c110ad.js"
+  "/Emojimeter/precache-manifest.dd2dd1d3edea988619e15da4603bcfb8.js"
 );
 
 workbox.clientsClaim();
