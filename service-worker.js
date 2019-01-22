f1911763f5f8a1bef7ec2497ece1fd66d8b8cde1
rajatrao777 <rajatrao777@gmail.com>
2019-01-23 00:11:27 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Emojimeter/precache-manifest.a01d6cf6a7568f032b0d7cfcaeb01533.js"
+  "/Emojimeter/precache-manifest.6531c069a6bf3044b0f1a47cca72a333.js"
 );
 
 workbox.clientsClaim();
