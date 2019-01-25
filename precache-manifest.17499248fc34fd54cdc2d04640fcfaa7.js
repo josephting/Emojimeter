4b388c3567bee6ffee6142aa7cc9137bf90657f3
rajatrao777 <rajatrao777@gmail.com>
2019-01-25 18:47:41 +0530

    Updates

---
+++ precache-manifest.17499248fc34fd54cdc2d04640fcfaa7.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = [
+  {
+    "revision": "4fdcfb1f074be1197f3abd0c0b864334",
+    "url": "/Emojimeter/static/media/e16.4fdcfb1f.png"
+  },
+  {
+    "revision": "61cdb01ae22b3801aacf",
+    "url": "/Emojimeter/static/css/main.5cf0d271.chunk.css"
+  },
+  {
+    "revision": "27727b42fc4e67ba697d",
+    "url": "/Emojimeter/static/js/1.27727b42.chunk.js"
+  },
+  {
+    "revision": "6708eb8ade55a32c6cdc",
+    "url": "/Emojimeter/static/js/runtime~main.6708eb8a.js"
+  },
+  {
+    "revision": "4b80bb5cf997a74cc69a17126ad43ef7",
+    "url": "/Emojimeter/static/media/e1.4b80bb5c.png"
+  },
+  {
+    "revision": "65aed047a1c34db18e23edabd68ee3f7",
+    "url": "/Emojimeter/static/media/e2.65aed047.png"
+  },
+  {
+    "revision": "303df98d59d2d438f6ed45dcaa29631b",
+    "url": "/Emojimeter/static/media/e3.303df98d.png"
+  },
+  {
+    "revision": "13b4ae4443c093d349dc6bce98fb6b2e",
+    "url": "/Emojimeter/static/media/e4.13b4ae44.png"
+  },
+  {
+    "revision": "51d74c37b00ee1085ab9edc47612990a",
+    "url": "/Emojimeter/static/media/e5.51d74c37.png"
+  },
+  {
+    "revision": "265c959dca39317fa1e3d758fd8b7649",
+    "url": "/Emojimeter/static/media/e6.265c959d.png"
+  },
+  {
+    "revision": "63dae88058ee1eb0b089e1a71aa1b158",
+    "url": "/Emojimeter/static/media/e7.63dae880.png"
+  },
+  {
+    "revision": "53a62f3a198241a4c3a9fa721bb9114f",
+    "url": "/Emojimeter/static/media/e8.53a62f3a.png"
+  },
+  {
+    "revision": "b37ca93f8a60639f7a407c0d919c3576",
+    "url": "/Emojimeter/static/media/e9.b37ca93f.png"
+  },
+  {
+    "revision": "2ef1a47be0eb6d4872b1d2c00d718311",
+    "url": "/Emojimeter/static/media/e10.2ef1a47b.png"
+  },
+  {
+    "revision": "26eaaf0f7219b2ae73cfda314b3eab9a",
+    "url": "/Emojimeter/static/media/e11.26eaaf0f.png"
+  },
+  {
+    "revision": "2f9beec87c9634bd38b97680659c8aa3",
+    "url": "/Emojimeter/static/media/e12.2f9beec8.png"
+  },
+  {
+    "revision": "b474bae714ccc6dfc305bf699e4a7ea8",
+    "url": "/Emojimeter/static/media/e13.b474bae7.png"
+  },
+  {
+    "revision": "782f8cee5f8673fc32c6792c0561d259",
+    "url": "/Emojimeter/static/media/e14.782f8cee.png"
+  },
+  {
+    "revision": "2f8f9615748f6bc87dfdece82d60fe01",
+    "url": "/Emojimeter/static/media/e15.2f8f9615.png"
+  },
+  {
+    "revision": "61cdb01ae22b3801aacf",
+    "url": "/Emojimeter/static/js/main.61cdb01a.chunk.js"
+  },
+  {
+    "revision": "59a261c721b775b3613617a8c0c00e1a",
+    "url": "/Emojimeter/static/media/e17.59a261c7.png"
+  },
+  {
+    "revision": "a027a00fe7eac6bb372d5700991b8dc4",
+    "url": "/Emojimeter/static/media/e18.a027a00f.png"
+  },
+  {
+    "revision": "7a91835fcd7500a93b72c9286135aac8",
+    "url": "/Emojimeter/static/media/e19.7a91835f.png"
+  },
+  {
+    "revision": "17b6c925ec99dc17d5ba5f0597c12ca6",
+    "url": "/Emojimeter/static/media/e20.17b6c925.png"
+  },
+  {
+    "revision": "2d09f7dac5fc73ae5b59d79c47d09dc7",
+    "url": "/Emojimeter/static/media/e21.2d09f7da.png"
+  },
+  {
+    "revision": "bf3e85209f1e03e7a916b018df471928",
+    "url": "/Emojimeter/static/media/e22.bf3e8520.png"
+  },
+  {
+    "revision": "8ff016a2b3ef1c8576c664a63b67caf3",
+    "url": "/Emojimeter/static/media/e23.8ff016a2.png"
+  },
+  {
+    "revision": "0eb275a0b969571ca235168b176949ed",
+    "url": "/Emojimeter/static/media/e24.0eb275a0.png"
+  },
+  {
+    "revision": "ccb112edb63776487554e0d11e2bf8ec",
+    "url": "/Emojimeter/static/media/e25.ccb112ed.png"
+  },
+  {
+    "revision": "bbf0dadab0fb0dfd806d1527ab0c2f4e",
+    "url": "/Emojimeter/static/media/e26.bbf0dada.png"
+  },
+  {
+    "revision": "0cc72ca4ae51505cbc277fdba852ba18",
+    "url": "/Emojimeter/static/media/e27.0cc72ca4.png"
+  },
+  {
+    "revision": "a4507f22f80321cd842af54a69d661e5",
+    "url": "/Emojimeter/static/media/e28.a4507f22.png"
+  },
+  {
+    "revision": "bc9653e6a3adfac10049fd4ac198cca1",
+    "url": "/Emojimeter/static/media/e29.bc9653e6.png"
+  },
+  {
+    "revision": "58beb49d6eca6ea38e1621177ccac356",
+    "url": "/Emojimeter/static/media/e30.58beb49d.png"
+  },
+  {
+    "revision": "eb28301e1357912732e802a6a83d9689",
+    "url": "/Emojimeter/static/media/e31.eb28301e.png"
+  },
+  {
+    "revision": "d5d15845366c69909f760a128f1e7cc3",
+    "url": "/Emojimeter/static/media/e32.d5d15845.png"
+  },
+  {
+    "revision": "39c831077c935e2f0e663601935353de",
+    "url": "/Emojimeter/static/media/e33.39c83107.png"
+  },
+  {
+    "revision": "5897294013cf044071f93980299a085f",
+    "url": "/Emojimeter/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Emojimeter/precache-manifest.0e770db02cb82f852aa5f53b6a8452a5.js"
+  "/Emojimeter/precache-manifest.17499248fc34fd54cdc2d04640fcfaa7.js"
 );
 
 workbox.clientsClaim();
